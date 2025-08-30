a31ceb6b6b6219cff94aa6df456d81efc5d17e6d
Shyam Jaiswal <sj1464@srmist.edu.in>
2025-08-30 06:38:29 +0530

    fix: production

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
+import { expressMiddleware } from '@apollo/server';
 import express from 'express';
 import cors from 'cors'; // Import cors
 
